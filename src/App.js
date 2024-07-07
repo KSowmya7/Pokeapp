c1d153909265ce875fa0be7d87e0313179eee9e1
KSowmya7 <kerellisowmya@gmail.com>
2024-07-07 22:48:05 +0530

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Pokedex from './components/Pokedex/Pokedex'
+import PokemonDetails from './components/PokemonDetails/PokemonDetails';
 
 function App() {
   return (
-    <>
-      pokeapp
-    </>
-    
+    <Routes>
+      <Route path="/" element={ <Pokedex/>}/>
+      <Route path='/pokemon/:id' element={<PokemonDetails/>}/>
+      <Route path='*' element={<h1>Not Found</h1>} />
+    </Routes>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/Search/Search.js
@@ -0,0 +1,15 @@
+import './Search.css';
+
+function Search(){
+  return(
+    <input 
+      id='search-pokemon'
+      type="text" 
+      placeholder='which pokemon you are looking for?' 
+    />
+  )
+
+
+}
+
+export default Search;
